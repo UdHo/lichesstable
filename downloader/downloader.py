02c16f4cab16dc29c3423d2c517605ac156da857
Udo Hoffmann <hoffmann.odu@gmail.com>
2022-05-12 19:50:22 +0200

    first part of games

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -9,13 +9,16 @@ class Downloader:
     baseurl = "https://lichess.org/api/"
     team = "schachfreunde-berlin-1903"
 
-    def get(self, url, cache = True):
+    def get(self, url, cache = True, decode = True):
         path = ''.join(e for e in url if e.isalnum())
         path = "cache/" + path
         if os.path.isfile(path):
             print("Using cached: " + url)
             f = open(path,"r")
-            return [json.loads(line) for line in iter(f.read().splitlines())]
+            if decode:
+                return [json.loads(line) for line in iter(f.read().splitlines())]
+            else:
+                return f.read()
         req = requests.get(url)
         print("Download: " + url)
         if req.status_code == 200:
@@ -23,7 +26,10 @@ class Downloader:
                 f = open(path,"w")
                 f.write(req.text)
                 f.close()
-            return [json.loads(line) for line in iter(req.text.splitlines())]
+            if decode:
+                return [json.loads(line) for line in iter(req.text.splitlines())]
+            else:
+                return req.text
         print(url + " download failed")
 
     def read_cached(self, filename):
@@ -40,6 +46,9 @@ class Downloader:
     def download_arena_result(self, tournament):
         result = self.get(self.baseurl + "tournament/" + tournament+ "/results")
         return result
+
+    def download_arena_games(self, tournament):
+        result = self.get(self.baseurl + "tournament/" + tournament+ "/games", True, False)
 
     def download_arena_team_result(self, tournament):
         result = self.get(self.baseurl + "tournament/" + tournament+ "/teams")
@@ -58,6 +67,7 @@ def get_data(team="schachfreunde-berlin-1903"):
     arenas = [a for a in filter(lambda a: ("Lichess Qua" in a["fullName"]) or ("Bundesliga" in a["fullName"]), downloader.download_team_arenas())]
     arena_team_results = [ downloader.download_arena_team_result(tournament) for tournament in map(lambda a: a["id"],arenas)]
     arena_results = [ downloader.download_arena_result(tournament) for tournament in map(lambda a: a["id"],arenas)]
+    games = [downloader.download_arena_games(tournament) for tournament in map(lambda a: a["id"], arenas)]
 
     arena_results =[downloader.read_cached(f) for f in glob.glob("cache/*tourna*results")]
 
